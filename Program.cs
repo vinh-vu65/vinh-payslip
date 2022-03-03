9d9cf2dbeaed921fe96c625993277fa14823d806
Vinh Vu <vinh.vu65@gmail.com>
2022-03-04 10:21:34 +1100

    Refactor validation methods in Main

--- Program.cs
+++ Program.cs
@@ -9,67 +9,50 @@ public class Program
         double yearlyIncome = 0;
         double superRate = 0;
         int payPeriod = 0;
-        bool firstnameIsValid = false;
-        bool surnameIsValid = false;
+        bool nameIsValid = false;
         bool salaryIsValid = false;
         bool superIsValid = false;
         bool payPeriodIsValid = false;
         
         Console.WriteLine("Welcome to the payslip generator! \n");
         
-        while (!firstnameIsValid)
+        do
         {
             Console.Write("Please enter your first name: ");
             userFirstname = Console.ReadLine();
-            if (!String.IsNullOrWhiteSpace(userFirstname))
-            {
-                firstnameIsValid = true;
-            } else 
-                Console.WriteLine("First name field cannot be empty.");
-        }
+            nameIsValid = ValidateName(userFirstname);
+        } while (!nameIsValid);
         
-        while (!surnameIsValid)
+        do
         {
             Console.Write("Please enter your surname: ");
             userSurname = Console.ReadLine();
-            if (!String.IsNullOrWhiteSpace(userSurname))
-            {
-                surnameIsValid = true;
-            } else 
-                Console.WriteLine("Surname field cannot be empty.");
-        }
+            nameIsValid = ValidateName(userSurname);
+        } while (!nameIsValid);
 
         while (!salaryIsValid) 
         {
             Console.Write("Please enter your annual salary: ");
             Double.TryParse(Console.ReadLine(), out yearlyIncome);
-            if (yearlyIncome > 0) 
-            {
-                salaryIsValid = true;
-            } else
-                Console.WriteLine("Please enter a number greater than zero.");
+            salaryIsValid = ValidateSalary(yearlyIncome);
         }
         
         while (!superIsValid)
         {
             Console.Write("Please enter your super rate (%): ");
             bool superValidate = Double.TryParse(Console.ReadLine(), out superRate);
-            if (superRate >= 0 && superRate <= 50 && (superValidate))
+            if (superValidate)
             {
-                superIsValid = true;
+                superIsValid = ValidateSuper(superRate);
             } else
-                Console.WriteLine("Please enter a number between 0 and 50.");
+                Console.WriteLine("-- Please enter a number between 0 and 50 -- \n");
         }
         
         while (!payPeriodIsValid)
         {
             Console.Write("Please enter pay period month as a number (1-12): ");
             Int32.TryParse(Console.ReadLine(), out payPeriod);
-            if (payPeriod >= 1 && payPeriod <= 12)
-            {
-                payPeriodIsValid = true;
-            } else
-                Console.WriteLine("Please enter a number between 1 and 12.");
+            payPeriodIsValid = ValidatePayPeriod(payPeriod);
         }
         
         User u = new User(userFirstname, userSurname, yearlyIncome, superRate);
@@ -77,5 +60,45 @@ public class Program
         Payslip p = new Payslip(u, t, payPeriod);
         p.DetermineMonth(payPeriod);
         p.DisplayPayslip();
+    }
+
+    public static bool ValidateName(string userInput)
+    {
+        if (!String.IsNullOrWhiteSpace(userInput))
+            {
+                return true;
+            } else 
+                Console.WriteLine("-- Name field cannot be empty -- \n");
+                return false;
+    }
+
+    public static bool ValidateSalary(double salary)
+    {
+        if (salary > 0) 
+            {
+                return true;
+            } else
+                Console.WriteLine("-- Please enter a number greater than zero -- \n");
+                return false;
+    }
+
+    public static bool ValidateSuper(double super)
+    {
+        if (super >= 0 && super <= 50)
+            {
+                return true;
+            } else
+                Console.WriteLine("-- Please enter a number between 0 and 50 -- \n");
+                return false;
+    }
+
+    public static bool ValidatePayPeriod(int month)
+    {
+        if (month >= 1 && month <= 12)
+            {
+                return true;
+            } else
+                Console.WriteLine("-- Please enter a number between 1 and 12 -- \n");
+                return false;
     }
 }
